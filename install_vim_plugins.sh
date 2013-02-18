002c5811323a644823c899cb079dea4c42d9888b
Doug McNish <git@stopavoiding.us>
2013-02-18 11:49:47 -0500

    Let's install Pathogen!

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+#[[ -x $(which curl) ]] || echo 'Derp not found'; exit
 
 [[ -z $DOTVIM_DIR ]] && DOTVIM_DIR=~/.vim
 
 BUNDLE_DIR=${DOTVIM_DIR}/bundle 
+AUTOLOAD_DIR="${DOTVIM_DIR}/autoload"
 
 plugins=( \
 	'https://github.com/scrooloose/syntastic.git' \
@@ -11,6 +13,12 @@ plugins=( \
 	'https://github.com/tpope/vim-fugitive.git'
 	)
 
+
+echo -n "Installing Pathogen: "
+mkdir -p ${AUTOLOAD_DIR} && cd ${AUTOLOAD_DIR}
+curl -Sso pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+echo "DONE"
 
 mkdir -p ${BUNDLE_DIR} && cd ${BUNDLE_DIR}
 
@@ -21,3 +29,4 @@ do
 	git clone $plugin > /dev/null 2>&1 
 	echo "DONE" 
 done
+
