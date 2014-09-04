0442a345babab3974a009f683a63f1c8550b8044
Doug McNish <git@mcnish.es>
2014-09-04 12:26:10 -0400

    Vundle stuff looks good. Removing last of Pathogen stuff.

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -9,30 +9,16 @@ AUTOLOAD_DIR="${DOTVIM_DIR}/autoload"
 [[ -z $VIM_RC ]] && VIM_RC="${HOME}/.vimrc"
 [[ -z $VUNDLE_RC ]] && VUNDLE_RC="${HOME}/.bundle.vim"
 
-#plugins=( \
-#	'git://github.com/altercation/vim-colors-solarized.git' \
-#	'https://github.com/scrooloose/syntastic.git' \
-#	'https://github.com/Lokaltog/vim-distinguished.git' \
-#	'https://github.com/tpope/vim-fugitive.git' \
-#	'https://github.com/Lokaltog/vim-powerline.git' \
-#	'https://github.com/kien/ctrlp.vim.git' \
-#	'https://github.com/scrooloose/nerdtree.git'
-#	)
-
+# Install anything we want to manually grab
+# before installing Vundle.vim
 plugins=( \
 	'https://github.com/gmarik/Vundle.vim.git'
 	)
 
 
-#echo -n "Installing Pathogen: "
-#mkdir -p ${AUTOLOAD_DIR} && cd ${AUTOLOAD_DIR}
 mkdir -p ${AUTOLOAD_DIR}
-#curl -Sso pathogen.vim \
-#    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-#echo "DONE"
 
 mkdir -p ${BUNDLE_DIR} && pushd ${BUNDLE_DIR}
-
 
 for plugin in ${plugins[*]} 
 do 
